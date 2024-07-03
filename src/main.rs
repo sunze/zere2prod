0422f7d1530a296efd2e06c751aa872fe791be5c
孙泽 <sunze1990@gmail.com>
2024-07-03 19:28:33 +0800

    first

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
     run()?.await
 }
---
+++ src/startup.rs
@@ -0,0 +1,16 @@
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use crate::greet;
+use crate::routes::{health_check, subscribe};
+
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .route("/", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8000")?
+        .run();
+    Ok(server)
+}
